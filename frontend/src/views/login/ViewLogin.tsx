cbff91cb401a3dafd96430ea44c877aa99580bda
Sebastian-H21 <sebastianhinojosa21@gmail.com>
2025-05-23 13:13:11 -0600

    Módulo Login

--- frontend/src/views/login/ViewLogin.tsx
+++ frontend/src/views/login/ViewLogin.tsx
@@ -1,18 +1,17 @@
 import Login from "../../components/Login";
 import Footer from "../../components/Footer";
+import ModoOscuro from "../../components/ModoOscuro";
 
+const ViewLogin = () => {
+    return (
+        <div className="relative min-h-screen overflow-hidden bg-white dark:bg-gray-900">
+        <ModoOscuro />
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <Login />
+            <Footer />
+        </div>
+        </div>
+    );
+};
 
-const ViewLogin = () =>{
-
-return (
-
-    <>    
-    <Login/>
-    <Footer/>
-    
-    </>
-
-
-);
-}
 export default ViewLogin;
