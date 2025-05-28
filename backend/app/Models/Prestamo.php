386fc298ad242574a29591c95b3acbabd782a408
Sebastian-H21 <sebastianhinojosa21@gmail.com>
2025-05-27 20:34:16 -0600

    Modificaciones para el uso de softdelete y relaciones

--- backend/app/Models/Prestamo.php
+++ backend/app/Models/Prestamo.php
@@ -6,18 +6,62 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-
 class Prestamo extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'fecha_prestamo',
         'fecha_devolucion',
+        'practica',
         'id_estudiante',
-        'rfc',
-        'id_material',
-        'estado',
+        'id_maestro',
+        'id_encargado',
+        'id_materia',
     ];
+
+    public function estudiante()
+    {
+        return $this->belongsTo(Estudiantes::class, 'id_estudiante', 'id')->withTrashed();
+    }
+
+    public function maestro()
+    {
+        return $this->belongsTo(Maestro::class, 'id_maestro', 'id')->withTrashed();
+    }
+
+    public function encargado()
+    {
+        return $this->belongsTo(Encargado::class, 'id_encargado')->withTrashed();
+    }
+
+    public function materiales()
+    {
+        return $this->belongsToMany(Material::class, 'detalle_prestamos', 'prestamo_id', 'material_id')->withTrashed();
+    }
+
+    public function materia()
+    {
+        return $this->belongsTo(Materia::class, 'id_materia', 'id')->withTrashed();
+    }
+
+    public function materialesDetalle()
+    {
+        return $this->hasMany(DetallePrestamo::class, 'prestamo_id', 'id')->withTrashed();
+    }
+
+    protected static function booted()
+    {
+        static::deleting(function ($prestamo) {
+            if ($prestamo->isForceDeleting()) {
+                $prestamo->materialesDetalle()->forceDelete();
+            } else {
+                $prestamo->materialesDetalle()->delete(); 
+            }
+        });
+    }
+
+
 }
+
+
