397f7e6a6145966c07053886fc7afcf4a7193d37
Sebastian-H21 <sebastianhinojosa21@gmail.com>
2025-06-10 01:54:02 -0600

    Modificación de la importación del componente ventana

--- frontend/src/components/Perfil.tsx
+++ frontend/src/components/Perfil.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"; 
-import ModalForm from "../components/Ventana"; 
+import ModalForm from "./Ventana"; 
 import api from "../utils/api"; 
 import { toast } from "react-toastify";
 
