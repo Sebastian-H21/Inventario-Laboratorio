be8ae48f0a7c52c701a47c2ddd49957d10700747
Sebastian-H21 <sebastianhinojosa21@gmail.com>
2025-06-13 10:06:54 -0600

    .

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./utils/queryClient";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
+
+// 5. Firma
 console.log(
-  `%c💻 Sistema desarrollado por T.S.H.M. –https://github.com/Sebastian-H21`,
+  `%c💻 Sistema desarrollado por T.S.H.M. – https://github.com/Sebastian-H21`,
   "color: #4f46e5; font-size: 14px; font-weight: bold;"
 );
 console.log(
-  "%c📅 11/06/2025 · 🛠️ Tecnologías: React, TypeScript, Laravel",
+  "%c📅 13/06/2025 · 🛠️ Tecnologías: React, TypeScript, Laravel",
   "color: #6b7280; font-size: 12px;"
 );
 console.log(
   "%c - “Wake me up when you need me”. - Master Chief 🎮",
   "color: #10b981; font-style: italic;"
 );
+
